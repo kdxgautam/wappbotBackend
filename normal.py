0c8e8b9c8cc8bd697b68ccb3e2fc0c5b691595d3
kdxgautam <kdxgautam@gmail.com>
2024-07-14 06:51:13 +0530

    api created

--- normal.py
+++ normal.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from twocaptcha import TwoCaptcha
 
-api_key="f4d81fdcd882e3222634088f4b6857ef"
+api_key="34c4bc29a7d7f813933968b6576d93b0"
 
 
 
@@ -14,24 +14,24 @@ solver = TwoCaptcha(api_key)
 
 
 
-# try:
-#     result = solver.normal(sys.argv[1])
-
-# except Exception as e:
-#     sys.stdout.flush()
-
-# else:
-#     print(str(result))
-#     sys.stdout.flush()
-
-
-
-
 try:
-    result = solver.normal('./captchas/downloadedimage.jpg')
+    result = solver.normal(sys.argv[1])
 
 except Exception as e:
-    sys.exit(e)
+    sys.stdout.flush()
 
 else:
-    sys.exit('result: ' + str(result))
+    print(str(result))
+    sys.stdout.flush()
+
+
+
+
+# try:
+#     result = solver.normal('./captchas/downloadedimage.jpg')
+
+# except Exception as e:
+#     sys.exit(e)
+
+# else:
+#     sys.exit('result: ' + str(result))
